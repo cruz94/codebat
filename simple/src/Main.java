e43eea8c7a7f6cf571bc25ffb21186d568c4f79f
praveencruz28@gmail.com <Deepriya2311*>
2024-07-18 09:13:50 +0530

    CB - mapBully

--- simple/src/Main.java
+++ simple/src/Main.java
@@ -1,21 +1,29 @@
 import com.leetcode.TwoSum;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        TwoSum ts = new TwoSum();
-        int[] ints = ts.twoSum(new int[]{2, 7, 8, 0}, 9);
-        System.out.println(Arrays.toString(ints));
+//        TwoSum ts = new TwoSum();
+//        int[] ints = ts.twoSum(new int[]{2, 7, 8, 0}, 9);
+//        System.out.println(Arrays.toString(ints));
 
 //        StringReverse stringRev = new StringReverse();
 //        System.out.println(stringRev.frontBack("code"));
 
 //        countYZ count = new countYZ();
 //        System.out.println(count.countYZ("!!day--yaz!!"));
+
+        Map rat = new HashMap<>();
+        rat.put("a","candy");
+        rat.put("b","man");
+        MapBully mapBully = new MapBully();
+        System.out.println(mapBully.mapBully(rat));
     }
 }
 
